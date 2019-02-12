957486857e4d124be708410470a782cb98c38be5
Kevin Kiningham <kkiningh@stanford.edu>
2019-02-11 21:20:45 -0800

    Simplify floparray to use single cycle

--- gcn/floparray.sv
+++ gcn/floparray.sv
@@ -14,30 +14,14 @@ module floparray #(
   input  logic             wen,
   input  logic [WIDTH-1:0] wdata
 );
-
-logic [$clog2(DEPTH)-1:0][WIDTH-1:0] mem;
-logic ren_q, wen_q;
-logic [$clog2(DEPTH)-1:0] raddr_q, waddr_q;
-logic [WIDTH-1:0] wdata_q;
-
-always_ff @ (posedge clock) begin
+  logic [$clog2(DEPTH)-1:0][WIDTH-1:0] mem;
+  always_ff @ (posedge clock) begin
     if (~reset_n) begin
-        raddr_q <= '0;
-        ren_q   <= 1'b0;
-        waddr_q <= '0;
-        wen_q   <= 1'b0;
+      rdata <= 'b0;
+    end else begin
+      rdata <= ren ? mem[raddr] : 'b0;
+      if (wen & ~ren)
+        mem[waddr_q] <= wdata;
     end
-    else begin
-        raddr_q <= raddr;
-        ren_q   <= ren;
-        waddr_q <= waddr;
-        wen_q   <= wen;
-
-        rdata <= ren_q ? mem[raddr_q] : 0;
-        mem[waddr_q] <= (wen_q & ~ren_q) ? wdata_q : mem[waddr_q]; // Or waddr != raddr
-    end
-end
-
-
-
+  end
 endmodule
