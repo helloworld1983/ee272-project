404347a4b4fdf52d08255d4a11db57fabfa875d3
Kevin Kiningham <kkiningh@stanford.edu>
2019-02-14 21:10:29 -0800

    Add reductionbuffer

---
+++ gcn/test/execute/reductionbuffer_test.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "gcn/test/verilator_driver.h"
+#include "gcn/test/execute/Vreductionbuffer/Vreductionbuffer.h"
+
+class ReductionBufferTest : public ::testing::Test {
+  protected:
+    VerilatorDUT<Vreductionbuffer> dut;
+
+    void write(
+
+    void TearDown() override { dut.finish(); }
+};
+
+TEST_F(ReductionBufferTest, Simple) {
+  dut.reset();
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  // Global verilator setup
+  Verilated::commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  Verilated::debug(0);
+  Verilated::randReset(0);
+
+  return RUN_ALL_TESTS();
+}
