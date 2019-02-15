c57a9c263e3f7b5a020361ec692c6859cdcb55c8
Ann Wu <wuyuanchih@gmail.com>
2019-02-14 22:57:33 -0800

    Updated logic for dram2gb_cntl.sv

--- gcn/dram2gb_cntl.sv
+++ gcn/dram2gb_cntl.sv
@@ -30,23 +30,23 @@ module dram2gb_cntl #(
 
   // -- Interface with GB --
   // Weight array interface
-  output [NUM_WGT_RBANK-1:0][$clog2(WGT_DEPTH)-1:0]                   wgt_raddr,
+  output [NUM_WGT_RBANK-1:0][BATCH_SIZE-1:0]                          wgt_raddr,
   output [NUM_WGT_RBANK-1:0][$clog2(NUM_WGT_RBANK+NUM_WGT_WBANK)-1:0] wgt_rsel, // Swap Arb :: know which index to assert
   output [NUM_WGT_RBANK-1:0][$clog2(NUM_WGT_RBANK+NUM_WGT_WBANK)-1:0] wgt_ren,
   input  [NUM_WGT_RBANK-1:0][WGT_WIDTH-1:0]                           wgt_rdata,
 
-  output [NUM_WGT_WBANK-1:0][$clog2(WGT_DEPTH)-1:0]                   wgt_waddr,
+  output [NUM_WGT_WBANK-1:0][BATCH_SIZE-1:0]                          wgt_waddr,
   output [NUM_WGT_WBANK-1:0][$clog2(NUM_WGT_RBANK+NUM_WGT_WBANK)-1:0] wgt_wsel, // Swap Arb :: know which index to assert
   output [NUM_WGT_WBANK-1:0][$clog2(NUM_WGT_RBANK+NUM_WGT_WBANK)-1:0] wgt_wen,
   output [NUM_WGT_WBANK-1:0][WGT_WIDTH-1:0]                           wgt_wdata,
 
   // Activation array interface
-  output [NUM_ACT_RBANK-1:0][$clog2(ACT_DEPTH)-1:0]                   act_raddr,
+  output [NUM_ACT_RBANK-1:0][BATCH_SIZE-1:0]                          act_raddr,
   output [NUM_ACT_RBANK-1:0][$clog2(NUM_ACT_RBANK+NUM_ACT_WBANK)-1:0] act_rsel, // Swap Arb :: know which index to assert
   output [NUM_ACT_RBANK-1:0][$clog2(NUM_ACT_RBANK+NUM_ACT_WBANK)-1:0] act_ren,
   input  [NUM_ACT_RBANK-1:0][ACT_WIDTH-1:0]                           act_rdata,
 
-  output [NUM_ACT_WBANK-1:0][$clog2(ACT_DEPTH)-1:0]                   act_waddr,
+  output [NUM_ACT_WBANK-1:0][BATCH_SIZE-1:0]                          act_waddr,
   output [NUM_ACT_WBANK-1:0][$clog2(NUM_ACT_RBANK+NUM_ACT_WBANK)-1:0] act_wsel, // Swap Arb :: know which index to assert
   output [NUM_ACT_WBANK-1:0][$clog2(NUM_ACT_RBANK+NUM_ACT_WBANK)-1:0] act_wen,
   output [NUM_ACT_WBANK-1:0][ACT_WIDTH-1:0]                           act_wdata,
@@ -68,13 +68,10 @@ module dram2gb_cntl #(
 
 logic writeback_gnt;
 logic writeback_req;
-
 logic writeback_done;
-logic writeback_active;
-
-
+logic writeback_active, writeback_active_q;
 logic load_done;
-logic load_active;
+logic load_active, load_active_q;
 
 // Retain knowledge of whether or not MAC had finished processing without writing back.
 always_ff @ (posedge clock) begin
@@ -83,16 +80,80 @@ always_ff @ (posedge clock) begin
   else if (writeback_gnt) writeback_req <= 1'b0;
 end
 
-// Arbitrate the address writing out to DRAM
+/* MEMORY INTERFACING LOGIC */
+// Static indices for each bank (Value is assigned by swap abritrator)
+localparam WGT_MAC_RD_BANK = 0;
+localparam WGT_MAC_WR_BANK = 1;
 
+localparam ACT_MAC_RD_BANK = 0;
+localparam ACT_MAC_WR_BANK = 1;
+localparam ACT_DRAM_BANK   = 2;
 
-// Arbitrate the address writing to SRAM
+logic [BATCH_SIZE*2-1:0] load_counter; // x2 because need to load both weights and activations
+logic [BATCH_SIZE-1:0]   writeback_counter;
+logic load_rise, writeback_rise;
+
+always_ff @ (posedge clock) begin
+  if (~reset_n) begin
+    load_active_q <= '0;
+    writeback_active_q <= '0;
+  end
+  else begin
+    load_active_q <= load_active;
+    writeback_active_q <= writeback_active;
+  end
+end
+assign load_rise = load_active & ~load_active_q;
+assign writeback_rise = writeback_active & ~writeback_active_q;
+
+always_ff @ (posedge clock) begin
+  if (~reset_n | load_rise | ~load_active | load_done)
+    load_counter <= '0;
+  else
+    load_counter <= load_counter + 1'b1;
+end
+always_ff @ (posedge clock) begin
+  if (~reset_n | writeback_rise | ~writeback_active | writeback_done)
+    writeback_counter <= '0;
+  else
+    writeback_counter <= writeback_counter + 1'b1;
+end
+
+// FIXME
+// 1. Activations get written from DRAM -> GB. Index the ADDR with the load counter.
+// 2. Weights get written from DRAM -> GB. Index the ADDR with the load counter - BATCH_SIZE
+always_ff @ (posedge clock) begin
+  if (load_active && (load_counter < BATCH_SIZE)) begin
+    rd_addr <= process_raddr[0]; //[load_counter];
+    act_waddr[ACT_MAC_RD_BANK] <= load_counter[BATCH_SIZE-1:0];
+    act_wen[ACT_MAC_RD_BANK] <= act_wsel[ACT_MAC_RD_BANK];
+    act_wdata[ACT_MAC_RD_BANK] <= rd_data[0]; //[load_counter[BATCH_SIZE-1:0]];
+  end
+  else if (load_active) begin
+    rd_addr <= process_raddr[0]; //[load_counter-BATCH_SIZE];
+    wgt_waddr[WGT_MAC_RD_BANK] <= load_counter[BATCH_SIZE-1:0]-BATCH_SIZE;
+    act_wen[WGT_MAC_RD_BANK] <= {1'b0,wgt_wsel[0]}; //[WGT_MAC_RD_BANK];
+    act_wdata[WGT_MAC_RD_BANK] <= rd_data[0]; //[load_counter[BATCH_SIZE-1:0]-BATCH_SIZE];
+  end
+end
+
+always_ff @ (posedge clock) begin
+  if (writeback_active) begin
+    // Activations get written from GB -> DRAM
+    wr_addr <= process_waddr[0]; //[load_counter];
+    act_raddr[ACT_MAC_RD_BANK] <= load_counter[BATCH_SIZE-1:0];
+    act_ren <= {2'b0,act_rsel[0]}; //[ACT_MAC_RD_BANK];
+    wr_data[0] <= act_rdata[0]; //[load_counter[BATCH_SIZE-1:0]];
+  end
+end
+
+assign writeback_done = (|writeback_counter) ? 1'b1 : 1'b0;
+assign load_done      = (|load_counter)      ? 1'b1 : 1'b0;
 
 dram2gb_stcntl dram2gb_stcntl
 (
   .*, // Clock/reset_n
 
-  // FIXME: temp tie-offs to compile, need to make correct connections
   .mac_done,
   .swap,
   .process_valid,
@@ -100,10 +161,10 @@ dram2gb_stcntl dram2gb_stcntl
 
   .writeback_req, // UNUSED ATM
   .writeback_gnt,
-  .writeback_done('0), // FIXME
-  .writeback_active(), // FIXME
-  .load_done('0), // FIXME
-  .load_active() // FIXME
+  .writeback_done,
+  .writeback_active,
+  .load_done,
+  .load_active
 );
 
 
