fa43547a8481b2e8729db312d360103bbb2a2c0d
Kevin Kiningham <kkiningh@stanford.edu>
2019-02-21 12:55:49 -0800

    SRAM port rename and datasheet fix

--- gcn/lib/sram/KW_ram_1rws_sram.sv
+++ gcn/lib/sram/KW_ram_1rws_sram.sv
@@ -13,24 +13,25 @@ module KW_ram_1rws_sram #(
   input logic reset_n,
 
   /* Port 1 control */
-  input logic p1_cs_n,
-  input logic p1_we_n,
-  input logic p1_re_n,
+  input logic cs_n,
+  input logic we_n,
+  input logic re_n,
 
   /* Port 1 datapath */
-  input  logic [ADDR_WIDTH-1:0] p1_addr,
-  input  logic [DATA_WIDTH-1:0] p1_data_in,
-  output logic [DATA_WIDTH-1:0] p1_data_out
+  input  logic [ADDR_WIDTH-1:0] rw_addr,
+  input  logic [DATA_WIDTH-1:0] data_in,
+  output logic [DATA_WIDTH-1:0] data_out
 );
   // Helper macro
 `define SRAM(n,w) \
   SRAM1RW``n``x``w sram_``n``x``w ( \
-    .A  (p1_addr),     \
-    .CE (clock),       \
-    .WEB(p1_we_n),     \
-    .OEB(p1_re_n),     \
-    .I  (p1_data_in),  \
-    .O  (p1_data_out)  \
+    .A  (rw_addr),  \
+    .CE (clock),    \
+    .WEB(we_n),     \
+    .OEB(re_n),     \
+    .CSB(cs_n),     \
+    .I  (data_in),  \
+    .O  (data_out)  \
   );
 
 `define if_SRAM(n,w) \
@@ -38,22 +39,22 @@ module KW_ram_1rws_sram #(
 
   // Instance the SRAMs
   generate
-    `if_SRAM(128,  8)
-    else `if_SRAM( 64, 32)
-    else `if_SRAM(256, 32)
-    else `if_SRAM(102, 84)
-    else `if_SRAM(512,  8)
-    else `if_SRAM(128, 48)
-    else `if_SRAM( 32, 50)
-    else `if_SRAM(256, 46)
-    else `if_SRAM(128, 46)
-    else `if_SRAM( 64,  8)
-    else `if_SRAM( 64,128)
-    else `if_SRAM(256,128)
-    else `if_SRAM(256,  8)
-    else `if_SRAM(256, 48)
-    else `if_SRAM(512,128)
-    else `if_SRAM(512, 32)
+    `if_SRAM( 128,  8)
+    else `if_SRAM(  64, 32)
+    else `if_SRAM( 256, 32)
+    else `if_SRAM(1024,  8) // Datasheet says 102x84, but 1024x8 is the correct cell name
+    else `if_SRAM( 512,  8)
+    else `if_SRAM( 128, 48)
+    else `if_SRAM(  32, 50)
+    else `if_SRAM( 256, 46)
+    else `if_SRAM( 128, 46)
+    else `if_SRAM(  64,  8)
+    else `if_SRAM(  64,128)
+    else `if_SRAM( 256,128)
+    else `if_SRAM( 256,  8)
+    else `if_SRAM( 256, 48)
+    else `if_SRAM( 512,128)
+    else `if_SRAM( 512, 32)
     else INVALID_INSTANCE requested_sram_module_not_defined();
   endgenerate
 `undef if_SRAM
--- gcn/lib/sram/KW_ram_2rws_sram.sv
+++ gcn/lib/sram/KW_ram_2rws_sram.sv
@@ -13,40 +13,42 @@ module KW_ram_2rws_sram #(
   input logic reset_n,
 
   /* Port 1 control */
-  input logic p1_cs_n,
-  input logic p1_we_n,
-  input logic p1_re_n,
+  input logic rw1_cs_n,
+  input logic rw1_we_n,
+  input logic rw1_re_n,
 
   /* Port 1 datapath */
-  input  logic [ADDR_WIDTH-1:0] p1_addr,
-  input  logic [DATA_WIDTH-1:0] p1_data_in,
-  output logic [DATA_WIDTH-1:0] p1_data_out,
+  input  logic [ADDR_WIDTH-1:0] rw1_addr,
+  input  logic [DATA_WIDTH-1:0] rw1_data_in,
+  output logic [DATA_WIDTH-1:0] rw1_data_out,
 
   /* Port 2 control */
-  input logic p1_cs_n,
-  input logic p1_we_n,
-  input logic p1_re_n,
+  input logic rw2_cs_n,
+  input logic rw2_we_n,
+  input logic rw2_re_n,
 
   /* Port 2 datapath */
-  input  logic [ADDR_WIDTH-1:0] p2_addr,
-  input  logic [DATA_WIDTH-1:0] p2_data_in,
-  output logic [DATA_WIDTH-1:0] p2_data_out
+  input  logic [ADDR_WIDTH-1:0] rw2_addr,
+  input  logic [DATA_WIDTH-1:0] rw2_data_in,
+  output logic [DATA_WIDTH-1:0] rw2_data_out
 );
   // Helper macro
 `define SRAM(n,w) \
   SRAM2RW``n``x``w sram_``n``x``w ( \
-    .A1  (p1_addr),     \
+    .A1  (rw1_addr),     \
     .CE1 (clock),       \
-    .WEB1(p1_we_n),     \
-    .OEB1(p1_re_n),     \
-    .I1  (p1_data_in),  \
-    .O1  (p1_data_out), \
-    .A2  (p2_addr),     \
+    .WEB1(rw1_we_n),     \
+    .OEB1(rw1_re_n),     \
+    .CSB1(rw1_cs_n),     \
+    .I1  (rw1_data_in),  \
+    .O1  (rw1_data_out), \
+    .A2  (rw2_addr),     \
     .CE2 (clock),       \
-    .WEB2(p2_we_n),     \
-    .OEB2(p2_oe_n),     \
-    .I2  (p2_data_in),  \
-    .O2  (p2_data_out)  \
+    .WEB2(rw2_we_n),     \
+    .OEB2(rw2_oe_n),     \
+    .CSB2(rw2_cs_n),     \
+    .I2  (rw2_data_in),  \
+    .O2  (rw2_data_out)  \
   );
 
 `define if_SRAM(n,w) \
