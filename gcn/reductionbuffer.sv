e9d880932ebcaf426e2f632259e68f26786e3836
Kevin Kiningham <kkiningh@stanford.edu>
2019-02-21 21:01:04 -0800

    Add new reduction buffer with SRAM macros

--- gcn/reductionbuffer.sv
+++ gcn/reductionbuffer.sv
@@ -1,39 +1,70 @@
 `default_nettype none
 
 module reductionbuffer (
-  input  wire clock,
-  input  wire reset_n,
+  input logic clock,
+  input logic reset_n,
 
-  // Control interface
-  input  wire sel, // Selects the accumulator buffer to READ
+  /* Control interface */
+  // NOTE: A swap occurs on the positive edge of clock after ready/valid
+  // are both 1
+  output logic swap_ready, // The accumulator buffer is ready to swap
+  input  logic swap_valid, // The accumulator buffer has a request to swap
 
   // Write interface
-  input  wire              wen,   // Enable writing to the activation buffer
-  input  wire [15:0][15:0] wdata, // Data input from the MAC array
+  input logic              w_en_n, // Enable writing to the activation buffer. Active low.
+  input logic [ 7:0]       w_addr, // Write address
+  input logic [15:0][15:0] w_data, // Data input from the MAC array
 
   // Read interface
-  input  wire              ren,   // Enable reading from the activation buffer
-  input  wire       [ 7:0] ridx,  // Which of the 256 rows to read from
-  output reg  [15:0][15:0] rdata  // The resulting activations
+  input  logic              r_en_n, // Read from the activation buffer. Active low.
+  input  logic [ 7:0]       r_addr, // Read address
+  output logic [15:0][15:0] r_data  // The resulting activations
 );
-  // main memory for the reduction values
-  reg [15:0][15:0][15:0] memory [0:1];
-  reg       [15:0][15:0] wbuff;
-
-  reg [7:0] widx_ff;
+  // Toggle the swap index when requested
+  logic [0:0] r_bank;
+  logic [0:0] w_bank;
   always_ff @(posedge clock or negedge reset_n) begin
     if (~reset_n) begin
-      widx_ff <= 8'b0;
-      memory  <= '{0, 0};
-    end else begin
-      if (wen) begin
-        // TODO: This is very hacky... we'll need to revisit for synthesis
-        memory[~sel][widx_ff] <= wdata + memory[~sel][widx_ff];
-        widx_ff <= widx_ff + 'b1;
-      end
-      if (ren) begin
-        rdata <= memory[sel][ridx];
-      end
+      r_bank <= 1'b0;
+      w_bank <= 1'b1;
+    end else if (swap_ready && swap_valid) begin
+      r_bank <= w_bank;
+      w_bank <= r_bank;
     end
   end
+  assign swap_ready = 1'b1;
+
+  // SRAM banks
+  localparam int SRAM_WIDTH = 128;
+  localparam int SRAM_DEPTH = 256;
+  localparam int SRAM_TILES = 256 / SRAM_WIDTH;
+  localparam int SRAM_ADDRW = $clog2(SRAM_DEPTH);
+  genvar i, j;
+  generate
+    for (i = 0; i < 2; i = i + 1) begin : BANK
+      wire bank_r_en_n = (r_bank == i) ? r_en_n : 1'b1;
+      wire bank_w_en_n = (w_bank == i) ? w_en_n : 1'b1;
+      wire [SRAM_ADDRW-1:0] bank_addr = !bank_r_en_n ? r_addr : w_addr;
+      wire [SRAM_TILES-1:0][SRAM_WIDTH-1:0] bank_data_in = w_data;
+      wire [SRAM_TILES-1:0][SRAM_WIDTH-1:0] bank_data_out;
+
+      for (j = 0; j < SRAM_TILES; j = j + 1) begin : SRAM
+        KW_ram_1rws_sram #(
+          .DATA_WIDTH(SRAM_WIDTH),
+          .DEPTH     (SRAM_DEPTH)
+        ) mem (
+          .clock   (clock),
+          .reset_n (reset_n),
+          .cs_n    (1'b0),
+          .we_n    (bank_w_en_n),
+          .re_n    (bank_r_en_n),
+          .rw_addr (bank_addr),
+          .data_in (bank_data_in [j]),
+          .data_out(bank_data_out[j])
+        );
+      end : SRAM
+    end : BANK
+  endgenerate
+
+  assign r_data = (r_bank == 0 ? BANK[0].bank_data_out : BANK[1].bank_data_out);
 endmodule
--- gcn/test/execute/reductionbuffer_test.cpp
+++ gcn/test/execute/reductionbuffer_test.cpp
@@ -11,53 +11,67 @@
 class ReductionBufferTest : public ::testing::Test {
   protected:
     VerilatorDUT<Vreductionbuffer> dut;
-    int sel_ = 0;
 
     void swapBuffers() {
-      sel_ = !sel_;
+      dut.poke(&Vreductionbuffer::swap_valid, 1);
+      dut.step();
+      dut.poke(&Vreductionbuffer::swap_valid, 0);
     }
 
-    void write(uint16_t (&data)[16]) {
-      dut.poke(&Vreductionbuffer::sel, sel_);
-      dut.poke(&Vreductionbuffer::wen, 1);
-      std::memcpy(dut.top.wdata, data, sizeof(uint16_t) * 16);
+    void write(uint16_t (&data)[16], uint8_t addr) {
+      dut.poke(&Vreductionbuffer::w_en_n, 0);
+      dut.poke(&Vreductionbuffer::w_addr, addr);
+      std::memcpy(dut.top.w_data, data, sizeof(uint16_t) * 16);
       dut.step();
-      dut.poke(&Vreductionbuffer::wen, 0);
+      dut.poke(&Vreductionbuffer::w_en_n, 1);
     }
 
-    void read(uint16_t (&data)[16]) {
-      dut.poke(&Vreductionbuffer::sel, sel_);
-      dut.poke(&Vreductionbuffer::ren, 1);
-      dut.poke(&Vreductionbuffer::ridx, 0);
+    void read(uint16_t (&data)[16], uint8_t addr) {
+      dut.poke(&Vreductionbuffer::r_en_n, 0);
+      dut.poke(&Vreductionbuffer::r_addr, addr);
       dut.step();
-      dut.poke(&Vreductionbuffer::ren, 0);
-      std::memcpy(data, dut.top.rdata, sizeof(uint16_t) * 16);
+      dut.poke(&Vreductionbuffer::r_en_n, 1);
+      std::memcpy(data, dut.top.r_data, sizeof(uint16_t) * 16);
     }
 
     void SetUp() override {
-      dut.poke(&Vreductionbuffer::sel, sel_);
-      dut.poke(&Vreductionbuffer::wen, 0);
-      dut.poke(&Vreductionbuffer::ren, 0);
+      dut.poke(&Vreductionbuffer::w_en_n, 1);
+      dut.poke(&Vreductionbuffer::r_en_n, 1);
       dut.reset();
     }
 
     void TearDown() override { dut.finish(); }
 };
 
-TEST_F(ReductionBufferTest, Simple) {
+TEST_F(ReductionBufferTest, Write0Read0) {
   // set to 0, 1, 2, ..., 15
   uint16_t data[16];
   std::iota(data, data + 16, 0);
+  write(data, 0);
+  swapBuffers();
+  read(data, 0);
+  for (int i = 0; i < 16; i++) {
+    EXPECT_EQ(data[i], i);
+  }
+}
+
+TEST_F(ReductionBufferTest, WriteAllReadAll) {
+  uint16_t data[16];
 
   // Do a bunch of writes
-  for (int i = 0; i < 15; i++) {
-    write(data);
+  for (int i = 0; i < 256; i++) {
+    std::iota(data, data + 16, 16 * i);
+    write(data, i);
   }
 
   swapBuffers();
-  read(data);
-  EXPECT_EQ(data[0], 0);
-  EXPECT_EQ(data[1], 1);
+
+  for (int i = 0; i < 256; i++) {
+    read(data, i);
+    for (int j = 0; j < 16; j++) {
+      EXPECT_EQ(data[j], i * 16 + j);
+    }
+  }
 }
 
 int main(int argc, char** argv) {
