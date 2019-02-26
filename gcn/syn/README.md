cc41885e97e03e495430cb85422e85f199662f5d
Kevin Kiningham <kkiningh@stanford.edu>
2019-02-25 20:58:43 -0800

    Add scripts for execute module

---
+++ gcn/syn/README.md
@@ -0,0 +1,7 @@
+To run synthesis, run the following from the `gcn/syn/` folder:
+
+```bash
+./dc.sh -f ../scripts/dc/execute.tcl
+```
+
+Note that you must add "../" before the path to the script file.
---
+++ gcn/syn/scripts/dc/execute/dc_setup.tcl
@@ -0,0 +1,8 @@
+puts "Info: Running script [info script]"
+
+# The name of the top-level design
+set DESIGN_NAME "execute"
+
+source ../scripts/dc/dc_setup.tcl
+
+puts "Info: Completed script [info script]"
